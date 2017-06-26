f888290d4deab605170e7b2e0d60e83cce4ac9d0
sabbas <syed.k.abbas1988@gmailcom>
2017-06-26 07:08:08 -0500

    Add additional colors and associated tests

--- escape/ansi.py
+++ escape/ansi.py
@@ -9,20 +9,81 @@ class ColorMixin(object):
     ansi color escape codes
     """
 
-    _template = u'\u001b[{start}m{text}\u001b[{end}m'
+    _template = u'\x1b[{start}m{text}\x1b[{end}m'
 
     def black(self):
-        pass
+        self._string = self._template.format(start=30, end=39, text=self._string)
+        return self
+
 
     def red(self):
         self._string = self._template.format(start=31, end=39, text=self._string)
+        return self
 
+    def bright_red(self):
+        self._string = self._template.format(start=91, end=39, text=self._string)
         return self
 
     def green(self):
-        self._string = u'{start}{text}{end}'
+        self._string = self._template.format(start=32, end=39, text=self._string)
+        return self
+
+    def bright_green(self):
+        self._string = self._template.format(start=92, end=39, text=self._string)
         return self
 
     def yellow(self):
-        self._string = u'{start}{text}{end}'
+        self._string = self._template.format(start=33, end=39, text=self._string)
         return self
+
+    def yellow_bright(self):
+        self._string = self._template.format(start=93, end=39, text=self._string)
+        return self
+
+    def blue(self):
+        self._string = self._template.format(start=34, end=39, text=self._string)
+        return self
+
+    def bright_blue(self):
+        self._string = self._template.format(start=94, end=39, text=self._string)
+        return self
+
+    def magenta(self):
+        self._string = self._template.format(start=35, end=39, text=self._string)
+        return self
+
+    def bright_magenta(self):
+        self._string = self._template.format(start=95, end=39, text=self._string)
+        return self
+
+    def cyan(self):
+        self._string = self._template.format(start=36, end=39, text=self._string)
+        return self
+
+    def bright_cyan(self):
+        self._string = self._template.format(start=96, end=39, text=self._string)
+        return self
+
+    def white(self):
+        self._string = self._template.format(start=37, end=39, text=self._string)
+        return self
+
+
+    def bright_white(self):
+        self._string = self._template.format(start=97, end=39, text=self._string)
+        return self
+
+    def gray(self):
+        self._string = self._template.format(start=90, end=39, text=self._string)
+        return self
+
+
+class BackGroundColorMixin(object):
+    """
+    A simple mixin class which wraps  input text between
+    ansi background color escape codes
+    """
+
+    _template = u'\u001b[{start}m{text}\u001b[{end}m'
+
+
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,13 +4,69 @@ Unittests for the core module
 from escape.core import Escape
 
 
-def test_string_gets_styled():
-    assert Escape('Hello World').red() == u'\u001b[31mHello World\u001b[39m'
-    # assert Escape('Hello World').blue() == u'\u001b[31mHello World\u001b[39m'
-    # assert Escape('Hello World').underline() == u'\u001b[31mHello World\u001b[39m'
-    # assert Escape('Hello World').bold() == u'\u001b[31mHello World\u001b[39m'
+def test_string_gets_styled_black():
+    assert Escape('hello').black() == u'\x1b[30mhello\x1b[39m'
+
+
+def test_string_gets_styled_red():
+    assert Escape('hello').red() == u'\x1b[31mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_red():
+    assert Escape('hello').bright_red() == u'\x1b[91mhello\x1b[39m'
+
+
+def test_string_gets_styled_green():
+    assert Escape('hello').green() == u'\x1b[32mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_green():
+    assert Escape('hello').bright_green() == u'\x1b[92mhello\x1b[39m'
+
+
+def test_string_gets_styled_yellow():
+    assert Escape('hello').green() == u'\x1b[33mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_yellow():
+    assert Escape('hello').bright_green() == u'\x1b[93mhello\x1b[39m'
+
+
+def test_string_gets_styled_blue():
+    assert Escape('hello').blue() == u'\x1b[34mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_blue():
+    assert Escape('hello').bright_blue() == u'\x1b[94mhello\x1b[39m'
+
+
+def test_string_gets_styled_magenta():
+    assert Escape('hello').magenta() == u'\x1b[35mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_magenta():
+    assert Escape('hello').bright_magenta() == u'\x1b[95mhello\x1b[39m'
+
+
+def test_string_gets_styled_cyan():
+    assert Escape('hello').cyan() == u'\x1b[36mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_cyan():
+    assert Escape('hello').bright_magenta() == u'\x1b[96mhello\x1b[39m'
+
+
+def test_string_gets_styled_white():
+    assert Escape('hello').white() == u'\x1b[37mhello\x1b[39m'
+
+
+def test_string_gets_styled_bright_white():
+    assert Escape('hello').bright_white * () == u'\x1b[97mhello\x1b[39m'
+
+
+def test_string_gets_styled_gray():
+    assert Escape('hello').gray() == u'\x1b[90mhello\x1b[39m'
 
 
 def test_multiple_styles_get_applied():
-    pass
-
+    assert Escape('Hello World').red().blue() == u'\x1b[34m\x1b[31mHello World\x1b[39m\x1b[39m'
