757e282f1c5ceebfa10d86dbf6d504999ad04bd1
sabbas <syed.k.abbas1988@gmailcom>
2017-07-02 12:04:17 -0500

    Prepare for pypi upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='escape',
+    packages=['escape'],  # this must be the same as the name above
+    version='1.0',
+    description='Simple Terminal Styling for Python',
+    author='Syed K Abbas',
+    author_email='syed.k.abbas1988@gmail.com',
+    url='https://github.com/skabbass1/escape',  # use the URL to the github repo
+    download_url='https://github.com/skabbass1/escape/archive/1.0.tar.gz',
+    keywords=[
+        'terminal-colors',
+        'ansi',
+        'terminal-emulators',
+        'ansi-escape-codes',
+        'command-line',
+        'console',
+        'color',
+        'cli',
+
+    ],  # arbitrary keywords
+    classifiers=[],
+)
