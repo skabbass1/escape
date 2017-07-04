7e00b7bdbf3a18aa398dbd20ba2efceada79ee08
sabbas <syed.k.abbas1988@gmailcom>
2017-07-03 20:56:59 -0500

    Minor refactoring

--- escape/ansi_styles.py
+++ escape/ansi_styles.py
@@ -2,6 +2,8 @@
 Ansi Color, BackGround Color and Modifier Mixins
 """
 
+_STYLE_TEMPLATE = u'\x1b[{start}m{text}\x1b[{end}m'
+
 
 class ColorMixin(object):
     """
@@ -9,70 +11,68 @@ class ColorMixin(object):
     ansi color escape codes
     """
 
-    _color_template = u'\x1b[{start}m{text}\x1b[{end}m'
-
     def black(self):
-        self._styled_string = self._color_template.format(start=30, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=30, end=39, text=self._styled_string)
         return self
 
     def red(self):
-        self._styled_string = self._color_template.format(start=31, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=31, end=39, text=self._styled_string)
         return self
 
     def bright_red(self):
-        self._styled_string = self._color_template.format(start=91, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=91, end=39, text=self._styled_string)
         return self
 
     def green(self):
-        self._styled_string = self._color_template.format(start=32, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=32, end=39, text=self._styled_string)
         return self
 
     def bright_green(self):
-        self._styled_string = self._color_template.format(start=92, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=92, end=39, text=self._styled_string)
         return self
 
     def yellow(self):
-        self._styled_string = self._color_template.format(start=33, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=33, end=39, text=self._styled_string)
         return self
 
     def bright_yellow(self):
-        self._styled_string = self._color_template.format(start=93, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=93, end=39, text=self._styled_string)
         return self
 
     def blue(self):
-        self._styled_string = self._color_template.format(start=34, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=34, end=39, text=self._styled_string)
         return self
 
     def bright_blue(self):
-        self._styled_string = self._color_template.format(start=94, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=94, end=39, text=self._styled_string)
         return self
 
     def magenta(self):
-        self._styled_string = self._color_template.format(start=35, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=35, end=39, text=self._styled_string)
         return self
 
     def bright_magenta(self):
-        self._styled_string = self._color_template.format(start=95, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=95, end=39, text=self._styled_string)
         return self
 
     def cyan(self):
-        self._styled_string = self._color_template.format(start=36, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=36, end=39, text=self._styled_string)
         return self
 
     def bright_cyan(self):
-        self._styled_string = self._color_template.format(start=96, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=96, end=39, text=self._styled_string)
         return self
 
     def white(self):
-        self._styled_string = self._color_template.format(start=37, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=37, end=39, text=self._styled_string)
         return self
 
     def bright_white(self):
-        self._styled_string = self._color_template.format(start=97, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=97, end=39, text=self._styled_string)
         return self
 
     def gray(self):
-        self._styled_string = self._color_template.format(start=90, end=39, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=90, end=39, text=self._styled_string)
         return self
 
 
@@ -82,70 +82,68 @@ class BackGroundColorMixin(object):
     ansi background color escape codes
     """
 
-    _background_color_template = u'\x1b[{start}m{text}\x1b[{end}m'
-
     def black_background(self):
-        self._styled_string = self._background_color_template.format(start=40, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=40, end=49, text=self._styled_string)
         return self
 
     def bright_black_background(self):
-        self._styled_string = self._background_color_template.format(start=100, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=100, end=49, text=self._styled_string)
         return self
 
     def red_background(self):
-        self._styled_string = self._background_color_template.format(start=41, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=41, end=49, text=self._styled_string)
         return self
 
     def bright_red_background(self):
-        self._styled_string = self._background_color_template.format(start=101, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=101, end=49, text=self._styled_string)
         return self
 
     def green_background(self):
-        self._styled_string = self._background_color_template.format(start=42, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=42, end=49, text=self._styled_string)
         return self
 
     def bright_green_background(self):
-        self._styled_string = self._background_color_template.format(start=102, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=102, end=49, text=self._styled_string)
         return self
 
     def yellow_background(self):
-        self._styled_string = self._background_color_template.format(start=43, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=43, end=49, text=self._styled_string)
         return self
 
     def bright_yellow_background(self):
-        self._styled_string = self._background_color_template.format(start=103, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=103, end=49, text=self._styled_string)
         return self
 
     def blue_background(self):
-        self._styled_string = self._background_color_template.format(start=44, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=44, end=49, text=self._styled_string)
         return self
 
     def bright_blue_background(self):
-        self._styled_string = self._background_color_template.format(start=104, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=104, end=49, text=self._styled_string)
         return self
 
     def magenta_background(self):
-        self._styled_string = self._background_color_template.format(start=45, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=45, end=49, text=self._styled_string)
         return self
 
     def bright_magenta_background(self):
-        self._styled_string = self._background_color_template.format(start=105, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=105, end=49, text=self._styled_string)
         return self
 
     def cyan_background(self):
-        self._styled_string = self._background_color_template.format(start=46, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=46, end=49, text=self._styled_string)
         return self
 
     def bright_cyan_background(self):
-        self._styled_string = self._background_color_template.format(start=106, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=106, end=49, text=self._styled_string)
         return self
 
     def white_background(self):
-        self._styled_string = self._background_color_template.format(start=47, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=47, end=49, text=self._styled_string)
         return self
 
     def bright_white_background(self):
-        self._styled_string = self._background_color_template.format(start=107, end=49, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=107, end=49, text=self._styled_string)
         return self
 
 
@@ -154,36 +152,35 @@ class ModifiersMixin(object):
     A simple mixin class which wraps  input text between
     ansi text modifier  escape codes
     """
-    _modifier_template = u'\x1b[{start}m{text}\x1b[{end}m'
 
     def reset(self):
-        self._styled_string = self._modifier_template.format(start=0, end=0, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=0, end=0, text=self._styled_string)
         return self
 
     def bold(self):
-        self._styled_string = self._modifier_template.format(start=1, end=22, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=1, end=22, text=self._styled_string)
         return self
 
     def dim(self):
-        self._styled_string = self._modifier_template.format(start=2, end=22, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=2, end=22, text=self._styled_string)
         return self
 
     def italic(self):
-        self._styled_string = self._modifier_template.format(start=3, end=23, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=3, end=23, text=self._styled_string)
         return self
 
     def inverse(self):
-        self._styled_string = self._modifier_template.format(start=7, end=27, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=7, end=27, text=self._styled_string)
         return self
 
     def hidden(self):
-        self._styled_string = self._modifier_template.format(start=8, end=28, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=8, end=28, text=self._styled_string)
         return self
 
     def strikethrough(self):
-        self._styled_string = self._modifier_template.format(start=9, end=29, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=9, end=29, text=self._styled_string)
         return self
 
     def underline(self):
-        self._styled_string = self._modifier_template.format(start=4, end=24, text=self._styled_string)
+        self._styled_string = _STYLE_TEMPLATE.format(start=4, end=24, text=self._styled_string)
         return self
--- escape/core.py
+++ escape/core.py
@@ -1,5 +1,5 @@
 """
-This module implements the core terminal styling functionality
+This module provides a simple class to manage styling strings in the terminal
 """
 
 from .ansi_styles import (
@@ -11,12 +11,12 @@ from .ansi_styles import (
 
 class Escape(ColorMixin, BackGroundColorMixin, ModifiersMixin, object):
     """
-
+    Simple string like class to produce ansi-escaped strings
     """
 
     def __init__(self, *args):
-        self._original = ''.join([str(arg) for arg in args])
-        self._styled_string = self._original
+        self._original_string = ''.join([str(arg) for arg in args])
+        self._styled_string = self._original_string
 
     def __eq__(self, other):
         return other == self._styled_string
@@ -28,7 +28,13 @@ class Escape(ColorMixin, BackGroundColorMixin, ModifiersMixin, object):
         return self.__str__()
 
     def __add__(self, other):
-        return self._styled_string + other
+        return self._styled_string + str(other)
 
     def __radd__(self, other):
-        return other + self._styled_string
+        return str(other) + self._styled_string
+
+    def __len__(self):
+        return len(self._original_string)
+
+    def __mul__(self, other):
+        return self._styled_string * other
