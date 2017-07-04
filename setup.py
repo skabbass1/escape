6b2f1d68d48a416f5f3e2ac5abe33fa6c8131cfe
sabbas <syed.k.abbas1988@gmailcom>
2017-07-04 11:30:12 -0500

    update version and move README to rst

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='escape',
     packages=['escape'],  # this must be the same as the name above
-    version='1.0',
+    version='1.1',
     description='Simple Terminal Styling for Python',
     author='Syed K Abbas',
     author_email='syed.k.abbas1988@gmail.com',
     url='https://github.com/skabbass1/escape',  # use the URL to the github repo
-    download_url='https://github.com/skabbass1/escape/archive/1.0.tar.gz',
+    download_url='https://github.com/skabbass1/escape/archive/1.1.tar.gz',
     license='MIT',
     keywords=[
         'terminal-colors',
