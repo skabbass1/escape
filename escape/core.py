56ff3b81ef619ab728744894ad525e790087917d
sabbas <syed.k.abbas1988@gmailcom>
2017-07-04 11:03:12 -0500

    Better unicode and python2 handling

--- escape/core.py
+++ escape/core.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
 """
 This module provides a simple class to manage styling strings in the terminal
 """
+
+import sys
 
 from .ansi_styles import (
     ColorMixin,
     BackGroundColorMixin,
     ModifiersMixin
 )
+
+PY3 = sys.version_info[0] >= 3
 
 
 class Escape(ColorMixin, BackGroundColorMixin, ModifiersMixin, object):
@@ -15,23 +20,35 @@ class Escape(ColorMixin, BackGroundColorMixin, ModifiersMixin, object):
     """
 
     def __init__(self, *args):
-        self._original_string = ''.join([str(arg) for arg in args])
+
+        self._original_string = ''.join(
+            [arg.encode('utf-8') if (not PY3 and isinstance(arg, unicode)) else str(arg) for arg in args]
+        )
         self._styled_string = self._original_string
 
-    def __eq__(self, other):
-        return other == self._styled_string
+    def __unicode__(self):
+        styled_string = self._styled_string
+        if isinstance(styled_string, bytes):
+            return styled_string.decode('utf8')
+        return styled_string
 
-    def __str__(self):
-        return self._styled_string
+    if PY3:
+        __str__ = __unicode__
+    else:
+        def __str__(self):
+            return self._styled_string
 
     def __repr__(self):
         return self.__str__()
 
+    def __eq__(self, other):
+        return other == self._styled_string
+
     def __add__(self, other):
-        return self._styled_string + str(other)
+        return self._styled_string + other
 
     def __radd__(self, other):
-        return str(other) + self._styled_string
+        return other + self._styled_string
 
     def __len__(self):
         return len(self._original_string)
